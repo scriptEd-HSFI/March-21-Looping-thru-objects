f8137a9aaafc38bff3b18dcbda32ceb797d9ed5a
Grace Tan <gracetan@Graces-MacBook-Pro.local>
2016-03-20 23:25:52 -0400

    excercise

---
+++ excercise.js
@@ -0,0 +1,16 @@
+var schedule = {
+ monday: { morning: "in school", evening: "doing homework" },
+ tuesday: { morning: "in school", evening: "at soccer practice"},
+ wednesday: { morning: "in school", evening: "at the club" }
+};
+
+Please loop through each day in the schedule object and print out:
+
+On [Monday], I will be:
+[in school] in the [morning]
+[doing homework] in the [evening]
+
+You will be printing out 3 lines per day.
+Hint: this requires two loops!
+
+
